0aa044788b0f78084ba50fddc1fa0f7306127472
karankotai <karankotai9183@gmail.com>
2022-03-31 16:08:46 +0530

    other changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
       <div className="logo-container">
         <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
-          <Link to="/">Cryptoverse</Link>
+          <Link to="/Cryptosphere">Cryptoverse</Link>
         </Typography.Title>
         <Button
           className="menu-control-container"
